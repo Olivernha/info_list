73009589e5f64b00f475a9c32cfdf7ec22484d99
oliver <nayhtetaung201733@gmail.com>
2020-09-08 21:57:36 +0630

    Finalized

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex justify-center text-gray-700">
+        Welcome to Info!
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log('Component mounted.')
+    }
+}
+</script>
